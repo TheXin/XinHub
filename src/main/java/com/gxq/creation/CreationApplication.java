b454598903b255206329b7760732117b6b6c4101
gxq <18260631324@163.com>
2019-06-18 23:27:48 +0800

    第二次

--- src/main/java/com/gxq/creation/CreationApplication.java
+++ src/main/java/com/gxq/creation/CreationApplication.java
@@ -2,12 +2,20 @@ package com.gxq.creation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CreationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CreationApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello 111";
     }
 
 }
